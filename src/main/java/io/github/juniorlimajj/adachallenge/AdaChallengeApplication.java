a9511fb5934b9527fe7eef3cca7abaf84e01b254
JuniorLima <josealbertorodriguesjunior@gmail.com>
2023-05-05 18:02:12 +0100

    initial structure

---
+++ src/main/java/io/github/juniorlimajj/adachallenge/AdaChallengeApplication.java
@@ -0,0 +1,13 @@
+package io.github.juniorlimajj.adachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/juniorlimajj/adachallenge/AdaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.juniorlimajj.adachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
